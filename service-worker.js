de7a22b4437754d9a4f814606a002515e5769978
Olumide Kolawole <ollyskay@gmail.com>
2024-11-04 18:09:25 +0100

    Initial commit for PrimeTech website project

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "prime-tech-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./script.js",
+  "./assets/img/icons/icon-144x144.png",
+  "./assets/img/icons/icon-152x152.png"
+];
+
+// Install Service Worker and Cache Assets
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Serve Cached Content When Offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Update Service Worker and Remove Old Caches
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
